de4ef16a0476ec8ac9d0ee15611e7f61f0abb886
Matthew Ryan <96961818+mryan5072@users.noreply.github.com>
2024-08-28 19:51:19 -0700

    fixes

--- game-site/next.config.mjs
+++ game-site/next.config.mjs
@@ -1,7 +1,5 @@
-// next.config.mjs
 export default {
-    images: {
-      domains: ['images.igdb.com'], // Add your image domains here
-    },
-  };
-  
+  images: {
+    domains: ['images.igdb.com'], // Add your image domains here
+  },
+};
