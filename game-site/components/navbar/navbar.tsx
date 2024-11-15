04b428a88fd9a9a6c5385eccb380e90909f9915f
Matthew Ryan <96961818+mryan5072@users.noreply.github.com>
2024-11-14 18:07:40 -0800

    Updated email display in navbar
    Now only shoes the username section of email when user is logged in.

--- game-site/components/navbar/navbar.tsx
+++ game-site/components/navbar/navbar.tsx
@@ -10,20 +10,20 @@ import "../../app/globals.css";
 const Navbar: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
+  const username = user?.email?.split("@", 1);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
     });
 
-    // Cleanup subscription on unmount
     return () => unsubscribe();
   }, []);
 
   const handleSignOut = async () => {
     try {
       await signOut(auth);
-      router.push('/'); // Redirect to home page after sign out
+      router.push('/');
     } catch (error) {
       console.error('Error signing out:', error);
     }
@@ -39,7 +39,7 @@ const Navbar: React.FC = () => {
         {user ? (
           <>
             <div className="user-info">
-              <span className="navbar-link">welcome, {user.email}</span>
+              <span className="navbar-link">welcome, {username}</span>
               <button onClick={handleSignOut} className="navbar-link sign-out-button">sign out</button>
             </div>
           </>
