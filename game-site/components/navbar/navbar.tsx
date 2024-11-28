90e43818f5d0137e9d6ccf8ea5677aea2566741a
Matthew Ryan <96961818+mryan5072@users.noreply.github.com>
2024-11-27 18:51:16 -0800

    Navbar changes
    -Migrated home page link into website title
    - Changed UI for when a user is logged in: now has a drop down menu on the user icon with a sign out option and a WIP account settings page.

--- game-site/components/navbar/navbar.tsx
+++ game-site/components/navbar/navbar.tsx
@@ -1,28 +1,38 @@
 "use client";
-
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { User, signOut } from 'firebase/auth';
 import { auth } from '../../app/firebase/config';
 import "../../app/globals.css";
+import { Menu, MenuItem, Avatar, Tooltip, IconButton, Divider, Typography, Box, ListItemIcon } from '@mui/material';
 
 const Navbar: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const router = useRouter();
-  const username = user?.email?.split("@", 1);
+  const username = user?.email?.split("@", 1)[0];
+  const open = Boolean(anchorEl);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
     });
-
     return () => unsubscribe();
   }, []);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleSignOut = async () => {
     try {
       await signOut(auth);
+      handleClose();
       router.push('/');
     } catch (error) {
       console.error('Error signing out:', error);
@@ -31,22 +41,64 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Game Tracker</div>
+      <div className="navbar-brand">
+        <Link href="/" className="navbar-title-link">Game Tracker</Link>
+      </div>
       <div className="navbar-links">
-        <Link href="/" className="navbar-link">home</Link>
         <Link href="/about" className="navbar-link">about</Link>
         <Link href="/mylist" className="navbar-link">my list</Link>
         {user ? (
-          <>
-            <div className="user-info">
-              <span className="navbar-link">welcome, {username}</span>
-              <button onClick={handleSignOut} className="navbar-link sign-out-button">sign out</button>
-            </div>
-          </>
+          <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+            <Tooltip title="account">
+              <IconButton
+                onClick={handleMenu}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+              >
+                <Avatar sx={{ width: 32, height: 32 }}>{username?.charAt(0).toUpperCase()}</Avatar>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  bgcolor: 'rgb(36, 25, 78)',
+                  color: 'white',
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem>
+                account settings
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={handleSignOut}>
+                sign out
+              </MenuItem>
+            </Menu>
+          </Box>
         ) : (
           <Link href="/login" className="navbar-link">log in</Link>
         )}
-        </div>
+      </div>
     </nav>
   );
 };
