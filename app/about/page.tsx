b2fb8cc6f03e9b2987db3956b6a45a5bd0d91b44
Matthew Ryan <96961818+mryan5072@users.noreply.github.com>
2024-12-20 22:19:01 -0800

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,28 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const About: React.FC = () => {
   return (
     <Box
       sx={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'flex-start',
+        alignItems: 'center',
         minHeight: '80vh',
         paddingTop: '80px',
         paddingBottom: '80px',
       }}
     >
+      <Typography
+        variant="h4"
+        sx={{
+          marginBottom: '20px',
+          textAlign: 'center',
+          color: 'gray',
+        }}
+      >
+        This page is a work in progress. Stay tuned for updates!
+      </Typography>
       <img
         src='/image0.jpg'
         alt="Your image description"
