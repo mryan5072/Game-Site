0d588dfb4dba90572795447ba2d5239ad0036fe3
Matthew Ryan <96961818+mryan5072@users.noreply.github.com>
2025-02-10 19:43:10 -0800

    Created my list page
    Created page that contains list of games user has reviewed.

---
+++ app/mylist/page.tsx
@@ -0,0 +1,11 @@
+import MyGamesList from "../../components/mygameslist/mygameslist";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <MyGamesList />
+    </div>
+  );
+};
+
+export default Home;
